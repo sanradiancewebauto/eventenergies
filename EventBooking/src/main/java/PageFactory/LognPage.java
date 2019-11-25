3bba7897b7e014fc5a77533e2c20b7f805ba85ef
Arif <arif.k@sanradiance.com>
2019-11-25 10:58:38 +0530

    login Changes

--- EventBooking/src/main/java/PageFactory/LognPage.java
+++ EventBooking/src/main/java/PageFactory/LognPage.java
@@ -1,79 +1,12 @@
-//package PageFactory;
-//
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//
-//public class LognPage {
-//	public static WebDriver driver;
-//	
-//	public static void login() throws Exception{
-//		driver = DriverManager.BaseDriver.getDriverConn("firefox");
-//		
-//		driver.manage().window().maximize();
-//		
-//		driver.findElement(By.id(ElementRepository.RegistrationPageElements.registerButton)).click();
-//	}
-//
-//}
-
-
 package PageFactory;
 
-//import java.io.FileInputStream;
+
 import java.io.IOException;
-//
-//import org.apache.poi.ss.usermodel.Cell;
-//import org.apache.poi.ss.usermodel.Row;
-//import org.apache.poi.xssf.usermodel.XSSFSheet;
-//import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LognPage {
 
-	static WebDriver driver;
-
-	static By Login = By.xpath(ElementRepository.LoginPageElements.Login);
-
-	static By LoginUserName = By.id(ElementRepository.LoginPageElements.LoginUserName);
-
-	static By LoginPassword = By.id(ElementRepository.LoginPageElements.LoginPassword);
-
-	static By loginButton = By.xpath(ElementRepository.LoginPageElements.loginButton);
-	
-	
-	
-
-	public LognPage(WebDriver driver) {
-		// TODO Auto-generated constructor stub
-		LognPage.driver= driver;
-	}
-
-
-	public WebDriver loginToEventEnergies(String uid,String pass) throws IOException{
-
-		driver.findElement(Login).click();
-
-		driver.findElement(LoginUserName).sendKeys(uid);
-
-		driver.findElement(LoginPassword).sendKeys(pass);
-
-		driver.findElement(loginButton).click();
-		
-		System.out.println("hhhhhhhhhhhhh");
-		
-		
-		
-		return driver;
-	}
-	
-
 
 	
 		}
-
-	
-
-
-
-
--- EventBooking/src/test/java/TestManager/loginTest.java
+++ EventBooking/src/test/java/TestManager/loginTest.java
@@ -1,88 +1,29 @@
-//
 package TestManager;
-//
-//import java.io.IOException;
-//import java.util.concurrent.TimeUnit;
-//
-////import java.util.concurrent.TimeUnit;
-//
-////import java.util.concurrent.TimeUnit;
-//
-//import org.junit.Test;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//
-//import PageFactory.LognPage;
-//
-//public class loginTest {
-//
-//	@Test
-//	public void verifyValidLogin() throws IOException {
-//
-//		System.setProperty("webdriver.chrome.driver", "D:\\chromedriver_win32\\chromedriver.exe");
-//		WebDriver driver= new ChromeDriver();
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		driver.manage().window().maximize();
-//		driver.get("https://www.eventenergies.com");
-//		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//		
-//	LognPage LognPage= new LognPage(driver);
-//		
-//		LognPage.loginToEventEnergies("lokeshbabu@gmail.com", "lokeshbabu@gmail.com");
-//
-//	}
-//
-//}
-//
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 
-
- 
-import java.io.FileInputStream;
-import java.io.IOException;
- 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.By;
- 
-//How to read excel files using Apache POI
 public class loginTest {
- public static void main (String [] args) throws IOException{
-                        //I have placed an excel file 'Test.xlsx' in my D Driver 
- FileInputStream fis = new FileInputStream("D:\\eventenergies\\EventBooking\\src\\test\\java\\TestManager\\Login .xlsx");
- 
- XSSFWorkbook workbook = new XSSFWorkbook(fis);
- 
- XSSFSheet sheet = workbook.getSheetAt(0);
- Row row = sheet.getRow(0);
- Cell cell = row.getCell(1);
- for(int i=1; i<=sheet.getLastRowNum(); i++) { 
-
-	// Import data for Email. 
-
-	cell = sheet.getRow(i).getCell(1); 
+	public static String browserName = "chrome";
+	public static WebDriver driver = null ;
 	
-	System.out.println(sheet.getRow(i).getCell(0));
-
-//	driver.findElement(By.id("email")).sendKeys(cell.getStringCellValue()); 
-
-	// Import data for password. 
-
-	cell = sheet.getRow(i).getCell(2); 
-	
-	System.out.println(sheet.getRow(i).getCell(1));
-
-
-
-
-//	driver.findElement(By.id("pass")).sendKeys(cell.getStringCellValue()); 
-
-	} 
-
-
- } 
+     
+    @BeforeTest
+    public void doBeforeTest() throws Exception {
+        System.out.println("Initialize setup before test run.Neelufar");
+        System.out.printf("Setting test browser as : %s ", browserName);
+        //LOGIN TO SITE
+        driver = DriverManager.BaseDriver.getDriverConn(browserName);
+    }
+    
+    @Test
+    public static void mainpage_click_login_test01() throws Exception
+    {
+    	System.out.println("hi");
+    }
+    
 }
-
-
